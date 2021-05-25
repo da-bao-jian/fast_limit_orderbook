e40d8cce392b67007715a4f94559fd6b3b0357f4
harry <gaohaoyuandd@gmail.com>
2021-05-24 23:28:59 -0400

    fixed logger

--- util/logger.py
+++ util/logger.py
@@ -1,12 +1,16 @@
 import logging, logging.handlers
 
-def setup_custom_logger(name, filename, level=logging.INFO):
+
+def _custom_logger(name, filename, level=logging.INFO):
     logger = logging.getLogger(name)
     logger.setLevel(level)
     formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(module)s - %(funcName)s - %(lineno)04d - %(message)s')
+
     console_handler = logging.StreamHandler()
     console_handler.setFormatter(formatter)
-    file_handler = logging.handlers.RotatingFileHandler(filename, maxBytes=2000000, backupCount=50)
+    file_handler = logging.handlers.RotatingFileHandler(filename, maxBytes=10000, backupCount=25)
     file_handler.setFormatter(formatter)
+
     logger.addHandler(file_handler)
+    logger.propagate = False
     return logger

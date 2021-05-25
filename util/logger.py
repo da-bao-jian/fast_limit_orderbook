ad45182580ad595fde0c3863d91bb01604004611
harry <gaohaoyuandd@gmail.com>
2021-05-24 23:02:35 -0400

    added logger

--- util/logger.py
+++ util/logger.py
@@ -1,12 +1,11 @@
 import logging, logging.handlers
 
-def setup_custom_logger(name, level=logging.INFO):
+def setup_custom_logger(name, filename, level=logging.INFO):
     logger = logging.getLogger(name)
     logger.setLevel(level)
     formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(module)s - %(funcName)s - %(lineno)04d - %(message)s')
     console_handler = logging.StreamHandler()
     console_handler.setFormatter(formatter)
-    filename = 'ftx_dn_bot_log-' + name + '.log'
     file_handler = logging.handlers.RotatingFileHandler(filename, maxBytes=2000000, backupCount=50)
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)

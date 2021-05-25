3947ae06b6300675e4f1fafa4bf397f33d666a58
harry <gaohaoyuandd@gmail.com>
2021-05-24 21:40:34 -0400

    created exchange handler class

---
+++ exchange_handlers/exchange_loop.py
@@ -0,0 +1,9 @@
+from util import logger
+
+class ExchangeLoop():
+    def __init__(self):
+        '''
+        This class handles event loops for exchanges' feeds
+        '''
+        self.feeds = []
+        

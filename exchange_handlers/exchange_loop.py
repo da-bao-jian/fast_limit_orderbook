26ff2b1d70f4518691f8a9f357d2d581e0bb4b37
harry <gaohaoyuandd@gmail.com>
2021-05-24 23:57:22 -0400

    added uvloop

--- exchange_handlers/exchange_loop.py
+++ exchange_handlers/exchange_loop.py
@@ -1,11 +1,14 @@
 from util import logger
+import asyncio
+import uvloop
 
-class ExchangeLoops():
+class ExchangeLoops:
     '''
-    This class initiates event loops and handles feeds
+    This class monitors event loops 
     '''
     def __init__(self):
         self.loops = []
+        asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
         
     def add_loop(self) -> None:
         return

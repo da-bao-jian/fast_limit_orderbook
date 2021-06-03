63dcf6a5dead5eb24f3112b654d25f8e9939e218
harry <gaohaoyuandd@gmail.com>
2021-06-03 01:23:14 -0400

    added pytest factory

--- tests/ob_tree_test.py
+++ tests/ob_tree_test.py
@@ -1,8 +1,10 @@
 import pytest
 from ..lob import LimitOrderBook
 from ..ob_tree.tree import LOBTree
-from ..ob_tree.order import OrderLinkedlist, Order
+from ..ob_tree.order import Order
+from ..ob_tree.orderlinkedlist import OrderLinkedlist
 import time
+import random
 
 @pytest.fixture(scope='module')
 def lob_instance():
@@ -16,10 +18,20 @@ def lobtree_instance():
 def orderll_instance():
     return OrderLinkedlist()
 
-@pytest.fixture
-def order_instance():
-    return Order(10000, '123456', time.time(), 1.2, 'bid')
-
+@pytest.fixture()
+def order_instances_factory():
+    '''
+    Pytest factory function that randomly generate Order instances each time it's being called
+    '''
+    price = random.randint(10000, 10100)
+    id = random.randint(100000,999999)
+    order_type = 'limit'
+    timestamp = time.time()
+    size = round(random.uniform(0.5, 2.5), 4)
+    side = ['bid', 'ask'][random.randrange(2)]
+    def _orders():
+        return Order(price=price, id=id, order_type=order_type, timestamp=timestamp, size=size, side=side)
+    return _orders
 
 def test_process_order(lob_instance, order_instance):
     '''

e235b47bbc73828933d75a308786fad0f30c3343
harry <gaohaoyuandd@gmail.com>
2021-06-04 13:29:52 -0400

    added new branch for only orderbook

--- exchange_handlers/exchanges/ftx.py
+++
@@ -1,8 +0,0 @@
-from ..feeds import Feed
-from ..defines import FTX
-class FTX(Feed):
-    id=FTX
-
-    def __init__(self, callbacks=None, **kwargs):
-        super().__init__('wss://ftexchange.com/ws/', callbacks=callbacks, **kwargs)
-        self.SYMBOL_ENDPOINT = "https://ftx.com/api/markets"
